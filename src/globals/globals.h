4f6e8deb2811e5f34895a81a9675006f050f4dd7
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-04-20 03:59:25 +0200

    Defined collector globals. Set 1second between each gather write.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -81,10 +81,13 @@
     #define DEFAULT_SERVICE_RELOADING_FILE "/.reloading"
     #define DEFAULT_SERVICE_VALIDATING_FILE "/.validating"
     #define DEFAULT_SS_SHUTDOWN_HOOK_FILE "/.shutdown"
+    #define DEFAULT_SS_PROCESS_DATA_COLLECTION_HOOK_FILE "/.data-collector"
+    #define DATA_COLLECTOR_SOCKET_FILE "/data-collector.sock"
 
     #define ONE_SECOND_OF_DELAY 1000000 // 1 second
     #define DEFAULT_SERVICE_PAUSE_INTERVAL 5000000 // 5 seconds
     #define BABYSITTER_TIMEOUT_INTERVAL 30000000 // 30 seconds
+    #define COLLECTOR_TIMEOUT_INTERVAL 1000000 // 1 second
     #define DEFAULT_GATHERING_PAUSE_MICROSECONDS 500000 // half a second
     #define DEFAULT_COUNT_OF_ROUNDS_OF_GATHERING 7200 // waiting half a second, hence 7200 is 60 minutes of gathering
     #define DEFAULT_USER_GROUP 0
