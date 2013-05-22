dc6bc02ffba2a6dd313cc8acaa0cbcca4ef932a4
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-05-22 18:37:29 +0200

    Defined commands for existing user services in Panel. More in details.
    * Defined status messages for all commands/ actions.
    * Defined svdss log level change commands: F1 (trace) .. F4(warn)
    * Defined commands below for highlighted services.
    * S - start
    * T - stop
    * C - configure
    * R - restart
    * V - validate
    * A - autostart toggle
    
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/panel/panel.cc
+++ src/panel/panel.cc
@@ -63,6 +63,7 @@ int main(int argc, char *argv[]) {
     refresh();
 
     int ch = 'p';
+    QString status = "Watching";
     int current_window_index = 0;
 
     QDir home(userHomeDir + SOFTWARE_DATA_DIR);
@@ -74,17 +75,19 @@ int main(int argc, char *argv[]) {
     init_pair(2, COLOR_GREEN, COLOR_BLACK);
     init_pair(3, COLOR_YELLOW, COLOR_BLACK);
     init_pair(4, COLOR_RED, COLOR_BLACK);
+    init_pair(5, COLOR_CYAN, COLOR_BLACK);
 
     attron(COLOR_PAIR(1));
     QString info = "Conrol Panel, version: " + QString(APP_VERSION) + ". " + QString(COPYRIGHT);
     mvprintw(0, 1, info.toUtf8());
-    mvprintw(0, 100, "Services: " + QString::number(APPS_NUMBER).toUtf8());
-    mvprintw(2, 1,
-  "Name                      PID    Address                Running?  Validating?  Configuring?  Installing?  Errors?  Autostart?");
-// moja-dowolna-apka-X1234   12345  lokalnydziad.dev:23456 YES       NO           NO            NO           NO
+    mvprintw(0, 90, "Services: " + QString::number(APPS_NUMBER).toUtf8());
+    mvprintw(3, 1, "Name                      PID    Address                Running?  Validating?  Configuring?  Installing?  Errors?  Autostart?");
     attroff(COLOR_PAIR(1));
 
     while (ch != 'q') {
+        QFileInfo cursorBaseDir = apps.at(current_window_index);
+        QString cursorAppDataDir = cursorBaseDir.absolutePath() + "/" + cursorBaseDir.baseName();
+
         switch (ch) {
             case KEY_UP:
                 if (current_window_index > 0)
@@ -99,6 +102,67 @@ int main(int argc, char *argv[]) {
                 else
                     current_window_index = APPS_NUMBER - 1;
                 break;
+
+            case KEY_F(1): /* Trace */
+                touch(QString(getenv("HOME")) + "/.trace");
+                status = "Triggered log level change to: Trace";
+                break;
+
+            case KEY_F(2): /* Debug */
+                touch(QString(getenv("HOME")) + "/.debug");
+                status = "Triggered log level change to: Debug";
+                break;
+
+            case KEY_F(3): /* Info */
+                touch(QString(getenv("HOME")) + "/.info");
+                status = "Triggered log level change to: Info";
+                break;
+
+            case KEY_F(4): /* Warn */
+                touch(QString(getenv("HOME")) + "/.warn");
+                status = "Triggered log level change to: Warning";
+                break;
+
+            case 'A': /* Autotart */ {
+                    QString autostartFile = cursorAppDataDir + "/.autostart";
+                    if (not QFile::exists(autostartFile))
+                        touch(autostartFile);
+                    else
+                        QFile::remove(autostartFile);
+                }
+                status = "Triggered autostart of application: " + cursorBaseDir.baseName();
+                break;
+
+            case 'S': /* Start */
+                touch(cursorAppDataDir + "/.start");
+                status = "Triggered start of application: " + cursorBaseDir.baseName();
+                break;
+
+            case 'T': /* sTop */
+                touch(cursorAppDataDir + "/.stop");
+                status = "Triggered stop of application: " + cursorBaseDir.baseName();
+                break;
+
+            case 'V': /* Validate */
+                touch(cursorAppDataDir + "/.validate");
+                status = "Triggered validation of application: " + cursorBaseDir.baseName();
+                break;
+
+            case 'I': /* Install */
+                touch(cursorAppDataDir + "/.install");
+                status = "Triggered installation of application: " + cursorBaseDir.baseName();
+                break;
+
+            case 'C': /* Configure */
+                touch(cursorAppDataDir + "/.configure");
+                status = "Triggered configuration of application: " + cursorBaseDir.baseName();
+                break;
+
+            case 'R': /* Restart */
+                touch(cursorAppDataDir + "/.restart");
+                status = "Triggered restart of application: " + cursorBaseDir.baseName();
+                break;
+
         }
 
         while (!kbhit()) {
@@ -113,56 +177,67 @@ int main(int argc, char *argv[]) {
                 }
 
                 /* name */
-                mvprintw(i + 3, 1, baseDir.baseName().toUtf8());
+                mvprintw(i + 4, 1, baseDir.baseName().toUtf8());
 
                 /* pid */
                 QString pid = QString(readFileContents(basePath + DEFAULT_SERVICE_PID_FILE).c_str()).trimmed();
                 if (pid.isEmpty())
-                    mvprintw(i + 3, 27, "NoPID");
+                    mvprintw(i + 4, 27, "-     ");
                 else
-                    mvprintw(i + 3, 27, pid.toUtf8());
+                    mvprintw(i + 4, 27, pid.toUtf8());
 
                 /* domain:port */
                 QString domain = QString(readFileContents(basePath + DEFAULT_SERVICE_DOMAIN_FILE).c_str()).trimmed();
                 QString port = QString(readFileContents(basePath + DEFAULT_SERVICE_PORTS_FILE).c_str()).trimmed();
-                mvprintw(i + 3, 34, (domain + ":" + port).toUtf8());
+                mvprintw(i + 4, 34, (domain + ":" + port).toUtf8());
 
                 /* running? */
                 if (QFile::exists(basePath + DEFAULT_SERVICE_RUNNING_FILE))
-                    mvprintw(i + 3, 57, "YES");
+                    mvprintw(i + 4, 57, "YES");
                 else
-                    mvprintw(i + 3, 57, "NO ");
+                    mvprintw(i + 4, 57, "NO ");
 
                 /* validating? */
                 if (QFile::exists(basePath + DEFAULT_SERVICE_VALIDATING_FILE))
-                    mvprintw(i + 3, 67, "YES");
+                    mvprintw(i + 4, 67, "YES");
                 else
-                    mvprintw(i + 3, 67, "NO ");
+                    mvprintw(i + 4, 67, "NO ");
 
                 /* configuring? */
                 if (QFile::exists(basePath + DEFAULT_SERVICE_CONFIGURING_FILE))
-                    mvprintw(i + 3, 80, "YES");
+                    mvprintw(i + 4, 80, "YES");
                 else
-                    mvprintw(i + 3, 80, "NO ");
+                    mvprintw(i + 4, 80, "NO ");
 
                 /* installing? */
                 if (QFile::exists(basePath + DEFAULT_SERVICE_INSTALLING_FILE))
-                    mvprintw(i + 3, 94, "YES");
+                    mvprintw(i + 4, 94, "YES");
                 else
-                    mvprintw(i + 3, 94, "NO ");
+                    mvprintw(i + 4, 94, "NO ");
 
                 /* errors? */
                 if (QFile::exists(basePath + DEFAULT_SERVICE_ERRORS_FILE))
-                    mvprintw(i + 3, 107, "YES");
+                    mvprintw(i + 4, 107, "YES");
                 else
-                    mvprintw(i + 3, 107, "NO ");
+                    mvprintw(i + 4, 107, "NO ");
 
                 /* autostart? */
                 if (QFile::exists(basePath + DEFAULT_SERVICE_AUTOSTART_FILE))
-                    mvprintw(i + 3, 116, "YES");
+                    mvprintw(i + 4, 116, "YES");
                 else
-                    mvprintw(i + 3, 116, "NO ");
+                    mvprintw(i + 4, 116, "NO ");
             }
+
+            /* print status - usually last command invoked */
+            attron(COLOR_PAIR(5));
+            QString statusContent = "Status: " + status;
+
+            /* a trick or hack, to clean screen after longer status messages in previous status content */
+            for (int ind = 1; ind < col; ind++) /* col is number of columns available on terminal */
+                statusContent += ' ';
+
+            mvprintw(1, 1, statusContent.toUtf8());
+            attroff(COLOR_PAIR(5));
 
             refresh();
             usleep(100000);
