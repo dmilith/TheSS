f440c1f9620c325c5ad83b6118f62e3ebd6e5275
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-06-06 21:25:23 +0200

    Added PoC Linux command which will soon init VPN network.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -26,7 +26,8 @@
     #define DEFAULT_USER_UID 501
     #define AMOUNT_OF_LOG_FILES_TO_KEEP 125
 
-    #define DEFAULT_VPN_INTERFACE_SETUP_COMMAND "kldunload if_tap ; kldload if_tap && ifconfig tap0 create && ifconfig tap0 10.10.0.1 netmask 255.255.255.0"
+    #define DEFAULT_VPN_INTERFACE_SETUP_COMMAND "kldunload if_tap ; kldload if_tap && ifconfig tap0 create && ifconfig tap0 10.10.0.1 netmask 255.255.0.0"
+    #define DEFAULT_VPN_INTERFACE_SETUP_COMMAND_LINUX "openvpn --mktun --dev tap0 && ifconfig tap0 10.20.0.1 netmask 255.255.0.0"
     #define DEFAULT_VPN_INTERFACE_SHUTDOWN_COMMAND "ifconfig tap0 destroy"
 
     #define SOFTWARE_DIR "/Software"
