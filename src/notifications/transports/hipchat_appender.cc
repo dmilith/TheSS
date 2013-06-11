61113b27f93da46ec1640a1b273d7d8ad268dbf4
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-06-11 15:15:38 +0200

    Refactored names of notification transports. Part I: Hipchat
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/notifications/transports/hipchat_appender.cc
+++ src/notifications/transports/hipchat_appender.cc
@@ -5,12 +5,11 @@
  *
  */
 
-#include "hipchat.h"
+#include "hipchat_notification.h"
 #include "hipchat_appender.h"
 
 
 void HipChatAppender::append(const QDateTime& timeStamp, Logger::LogLevel logLevel, const char* file, int line,
                              const char* function, const QString& message) {
-    //std::cerr << qPrintable(formattedString(timeStamp, logLevel, file, line, function, message));
-    notification(qPrintable(formattedString(timeStamp, logLevel, file, line, function, message)));
+    notification(qPrintable(formattedString(timeStamp, logLevel, file, line, function, message)), HIPCHAT);
 }
