60d59b1d625ba018025107a30d4896320bd2e310
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-06-24 13:44:58 +0200

    Fixed build for root when building with Sofin.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -2,7 +2,7 @@
 #define __GLOBALS__
 
     /* global constants */
-    #define APP_VERSION "0.42.5"
+    #define APP_VERSION "0.42.6"
     #define COPYRIGHT "Copyright © 2o13 VerKnowSys.com - All Rights Reserved."
 
     /* default BSD case: */
