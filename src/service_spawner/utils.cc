764fda775cf4e4ffffc3730e92ef71305ea68db3
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-07-10 18:17:51 +0200

    Fixed not closed file descriptor on bind check in registered UDP port check.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/service_spawner/utils.cc
+++ src/service_spawner/utils.cc
@@ -397,15 +397,16 @@ uint registerFreeUdpPort(uint specificPort) {
 
     int sockfd;
     struct sockaddr_in servaddr;
-    sockfd=socket(AF_INET,SOCK_DGRAM,0);
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
 
-    bzero(&servaddr,sizeof(servaddr));
+    bzero(&servaddr, sizeof(servaddr));
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = htonl(INADDR_ANY);
     servaddr.sin_port = htons(port);
-    if (bind(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr))) {
+    if (bind(sockfd, (struct sockaddr *)&servaddr, sizeof(servaddr))) {
         return registerFreeUdpPort(10000 + rand);
     }
+    close(sockfd);
     return port;
 }
 
