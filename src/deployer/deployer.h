52be27b7644e8f4518084260e1686ad17a99ba2e
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-12-07 18:39:35 +0100

    Cleanup in deployer.h API.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/deployer/deployer.h
+++ src/deployer/deployer.h
@@ -9,25 +9,16 @@
 #define __DEPLOYER_H__
 
 
-// #include "../service_spawner/file_events_manager.h"
-// #include "../service_spawner/service_config.h"
-// #include "../service_spawner/service_watcher.h"
-// #include "../jsoncpp/json/json.h"
-#include "../service_spawner/service_config.h"
 #include "../service_spawner/utils.h"
-#include "../service_spawner/service.h"
 #include "../service_spawner/process.h"
 #include "webapp_types.h"
-// #include "../service_spawner/service.h"
-// #include "../service_spawner/data_collector.h"
 
 #include <QObject>
-// #include <QFile>
-// #include <QEventLoop>
 
 
 void installDependencies();
 void cloneRepository(QString& sourceRepositoryPath, QString& serviceName, QString& branch, QString& stage);
 void createEnvironmentFiles(QString& serviceName, QString& domain, QString& stage, QString& branch);
+QString generateIgniterDepsBase(QString& latestReleaseDir, QString& serviceName, QString& branch, QString& domain);
 
 #endif
