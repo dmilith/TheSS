71c4a0c04530a29ce01f2178407ec01e89efa71f
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-12-12 19:10:17 +0100

    svdply doesn't require MODE=setup for 1st deploy. It will just detect it and automatically execute setup steps if there's no repository to push to.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -10,7 +10,7 @@
     #define logFatal LOG_FATAL
 
     /* global constants */
-    #define APP_VERSION "0.66.6"
+    #define APP_VERSION "0.66.7"
     #define COPYRIGHT "Copyright © 2o13 VerKnowSys.com - All Rights Reserved."
 
     /* default BSD case: */
