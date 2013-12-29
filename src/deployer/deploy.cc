8d6b6ffd5ac49955a92f9b919842b6dd9b56ba81
Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>
2013-12-29 13:09:39 +0100

    One more experimental change to error handling of PhpSite.
    Signed-off-by: Daniel (dmilith) Dettlaff <dmilith@verknowsys.com>

--- src/deployer/deploy.cc
+++ src/deployer/deploy.cc
@@ -383,6 +383,7 @@ server { \n\
         fastcgi_param PATH_INFO $fastcgi_script_name; \n\
         fastcgi_pass " + serviceName + "-" + stage + "; \n\
         fastcgi_intercept_errors on; \n\
+        error_log \"" + getServiceDataDir(serviceName) + DEFAULT_SERVICE_LOG_FILE + "\"; \n\
         error_page 400 402 403 404 502 503 504 = error.html; \n\
     } \n\
     location /error.html { \n\
