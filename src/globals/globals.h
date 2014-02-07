2a57762df7c172cd3abd665dc97ba17d897d8806
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-02-07 15:31:41 +0100

    Use zip instead of Apple tar to create archives. This prevents issues with ACL recognized as regular files on remote system. Zip utility is required (installed by default on OSX)

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -10,7 +10,7 @@
     #define logFatal LOG_FATAL
 
     /* global constants */
-    #define APP_VERSION "0.70.2"
+    #define APP_VERSION "0.70.3"
     #define COPYRIGHT "(c) 2o13-2o14 verknowsys.com"
 
     /* default BSD case: */
