917168c8c5f6f42df65b90dce67e080205870481
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-02-12 17:37:41 +0100

    This might break things and it's possibly not backward compatible.

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -10,7 +10,7 @@
     #define logFatal LOG_FATAL
 
     /* global constants */
-    #define APP_VERSION "0.72.1"
+    #define APP_VERSION "0.72.2"
     #define COPYRIGHT "(c) 2o13-2o14 verknowsys.com"
 
     /* default BSD case: */
