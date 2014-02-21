cf5392d34d8ed51718b271a87700a8fd8cd70b0c
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-02-21 03:04:49 +0100

    Support -d switch for DeathWatcher. By default disable debug output.

--- src/death_watch/death_watcher.cc
+++ src/death_watch/death_watcher.cc
@@ -13,12 +13,18 @@ int main(int argc, char *argv[]) {
     QCoreApplication app(argc, argv);
     QTextCodec::setCodecForCStrings(QTextCodec::codecForName(DEFAULT_STRING_CODEC));
     QStringList args = app.arguments();
+    QRegExp rxEnableDebug("-d");
 
     /* Logger setup */
     ConsoleAppender *consoleAppender = new ConsoleAppender();
     Logger::registerAppender(consoleAppender);
     consoleAppender->setFormat("%t{dd-HH:mm:ss} [%-7l] <%c:(%F:%i)> %m\n");
-    bool debug = true, trace = true;
+    bool debug = false, trace = false;
+    for (int i = 1; i < args.size(); ++i) {
+        if (rxEnableDebug.indexIn(args.at(i)) != -1 ) {
+            debug = true;
+        }
+    }
     if (trace && debug)
         consoleAppender->setDetailsLevel(Logger::Trace);
     else if (debug && !trace)
@@ -33,11 +39,11 @@ int main(int argc, char *argv[]) {
         logInfo() << "You need to specify pid to put a death watch on.";
         return 1;
     }
-    logInfo() << "DeathWatch version" << APP_VERSION << ". " << COPYRIGHT;
+    logInfo() << "DeathWatch version" << APP_VERSION << COPYRIGHT;
     bool ok = false;
     pid_t pid = args.at(1).toInt(&ok, 10);
     if (ok) {
-        logInfo() << "Death watch is in harvest mode for pid:" << QString::number(pid);
+        logInfo() << "Death watch is harvesting soul of pid:" << QString::number(pid);
         deathWatch(pid);
     } else {
         logError() << "Bad pid number given:" << args.at(1) << " Aborting.";
