e11056ce66584480ec776b5ece4a1427ddee8c2a
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-02-21 02:07:49 +0100

    Use different extension for low level service logger

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -122,7 +122,7 @@
     #define NOTIFICATIONS_DATA_DIR "/.notifications/"
     #define NOTIFICATION_MANAGER_LOG_FILE "/.notifications-manager.log"
     #define DEFAULT_SERVICE_PORT_NUMBER "0"
-    #define DEFAULT_SERVICE_OUTPUT_FILE (DEFAULT_SERVICE_LOG_FILE ".raw")
+    #define DEFAULT_SERVICE_OUTPUT_FILE (DEFAULT_SERVICE_LOG_FILE "-stderr.log")
     #define DEFAULT_SS_SHUTDOWN_FILE ".shutdown"
     #define DEFAULT_SS_SHUTDOWN_HOOK_FILE ("/" DEFAULT_SS_SHUTDOWN_FILE)
     #define DEFAULT_SS_GRACEFUL_SHUTDOWN_FILE ".shutdownGracefully"
