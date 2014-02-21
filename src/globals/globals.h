acc8a364ed39bf258466674b82173fc1cb341711
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-02-21 15:13:10 +0100

    Don't create two networks where only one is required.

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -36,7 +36,7 @@
     #define DEFAULT_USER_UID 501
     #define AMOUNT_OF_LOG_FILES_TO_KEEP 125
 
-    #define DEFAULT_VPN_INTERFACE_SETUP_COMMAND "kldunload if_tap ; kldload if_tap && ifconfig tap0 create &&  ifconfig tap1 create && ifconfig tap0 10.10.0.1 netmask 255.255.0.0"
+    #define DEFAULT_VPN_INTERFACE_SETUP_COMMAND "kldunload if_tap ; kldload if_tap && ifconfig tap0 create" /* &&  ifconfig tap1 create && ifconfig tap0 10.10.0.1 netmask 255.255.0.0" */
     #define DEFAULT_VPN_INTERFACE_SHUTDOWN_COMMAND "ifconfig tap0 destroy"
 
     #define SOFTWARE_DIR "/Software/"
