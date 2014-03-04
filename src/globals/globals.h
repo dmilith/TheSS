e589715fbad3fbb1576adaa14f64d205e6048e03
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-03-04 03:59:40 +0100

    Version bump. It's stable. Almost production ready.

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -15,7 +15,7 @@
     #define logFatal LOG_FATAL
 
     /* global constants */
-    #define APP_VERSION "0.83.3"
+    #define APP_VERSION "0.84.0"
     #define COPYRIGHT "(c) 2o13-2o14 verknowsys.com"
 
     /* default BSD case: */
