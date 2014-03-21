d48a304f6e04d5f451c8e94b2dfaff679bfe5552
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-03-21 02:09:54 +0100

    Version bump. Comment headers fixed.

--- src/globals/globals.h
+++ src/globals/globals.h
@@ -15,7 +15,7 @@
     #define logFatal LOG_FATAL
 
     /* global constants */
-    #define APP_VERSION "0.86.3"
+    #define APP_VERSION "0.86.4"
     #define COPYRIGHT "(c) 2o13-2o14 verknowsys.com"
 
     /* default BSD case: */
