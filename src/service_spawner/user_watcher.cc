4512022d798e20e8188406e3166aeca22052a80f
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-03-22 23:20:26 +0100

    Avoid "Default" service spawn.

--- src/service_spawner/user_watcher.cc
+++ src/service_spawner/user_watcher.cc
@@ -100,7 +100,7 @@ void SvdUserWatcher::collectServices() {
     logDebug() << "Current list of services:" << services;
 
     Q_FOREACH(QString name, services) {
-        if (not oldServices.contains(name) and not name.endsWith(DEFAULT_SERVICE_DISABLED_POSTFIX)) {
+        if (not oldServices.contains(name) and not name.endsWith(DEFAULT_SERVICE_DISABLED_POSTFIX) and not (name == "Default")) {
             logInfo() << "Initializing watchers for data dir of service:" << name;
 
             /* detect user watcher */
