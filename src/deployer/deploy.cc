c29504d81f159a7d293ed634d14c92853de8421b
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-04-12 13:35:02 +0200

    Commented out unused deploy code.

--- src/deployer/deploy.cc
+++ src/deployer/deploy.cc
@@ -924,15 +924,15 @@ QStringList filterSpawnableDependencies(const QString& deps) {
 }
 
 
-void startWithoutDependencies(const QString& servicePath) {
-    logInfo() << "Launching service without dependencies, using newly generated igniter.";
-    if (not QFile::exists(servicePath + DEFAULT_SERVICE_RUNNING_FILE))
-        touch(servicePath + START_WITHOUT_DEPS_TRIGGER_FILE);
-    else {
-        QFile::remove(servicePath + DEFAULT_SERVICE_RUNNING_FILE);
-        touch(servicePath + START_WITHOUT_DEPS_TRIGGER_FILE);
-    }
-}
+// void startWithoutDependencies(const QString& servicePath) {
+//     logInfo() << "Launching service without dependencies, using newly generated igniter.";
+//     if (not QFile::exists(servicePath + DEFAULT_SERVICE_RUNNING_FILE))
+//         touch(servicePath + START_WITHOUT_DEPS_TRIGGER_FILE);
+//     else {
+//         QFile::remove(servicePath + DEFAULT_SERVICE_RUNNING_FILE);
+//         touch(servicePath + START_WITHOUT_DEPS_TRIGGER_FILE);
+//     }
+// }
 
 
 void createEnvironmentFiles(QString& serviceName, QString& domain, QString& stage, QString& branch) {
