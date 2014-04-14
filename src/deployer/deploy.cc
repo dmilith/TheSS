f3149be754589947cb71901b57b91480d19cf218
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-04-14 09:48:02 +0200

    Re-enabled dynamic ports for web apps.

--- src/deployer/deploy.cc
+++ src/deployer/deploy.cc
@@ -577,12 +577,12 @@ void generateServicePorts(QString servicePath, int amount) {
     getOrCreateDir(portsDir);
     QString portFilePath = portsDir + DEFAULT_SERVICE_PORT_NUMBER; /* default port */
     QTime midnight(0, 0, 0);
-    if (not QFile::exists(portFilePath)) { // TODO: re enable dynamic ports
+    // if (not QFile::exists(portFilePath)) { // TODO: re enable dynamic ports
         qsrand(midnight.msecsTo(QTime::currentTime()));
         uint port = registerFreeTcpPort(abs((qrand() + 1024) % 65535));
         logInfo() << "Generated service port:" << QString::number(port);
         writeToFile(portFilePath, QString::number(port));
-    }
+    // }
     for (int i = 2; i < amount + 1; i++) {
         QString backupPortFilePath = portsDir + QString::number(i - 1);
         qsrand(midnight.msecsTo(QTime::currentTime()));
