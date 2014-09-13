1949641ada844c8fd8e45f79157b205f6e1f068d
Daniel (dmilith) Dettlaff <dmilith@me.com>
2014-09-13 18:54:44 +0200

    Show pid in Panel.

--- src/panel/gui.cc
+++ src/panel/gui.cc
@@ -201,11 +201,11 @@ void PanelGui::displayStatus(){
     /* SS status info */
     if(panel->isSSOnline()){
         wattron(mainWindow, C_SS_STATUS_ON);
-        mvwprintw(mainWindow, 0, 48, (" svdss:  ONLINE (" + QHostInfo::localHostName() + ")").toUtf8());
+        mvwprintw(mainWindow, 0, 48, (" svdss[" + QString::number(getuid()) + "]:  ONLINE (" + QHostInfo::localHostName() + ")").toUtf8());
         wattroff(mainWindow, C_SS_STATUS_ON);
     } else {
         wattron(mainWindow, C_SS_STATUS_OFF);
-        mvwprintw(mainWindow, 0, 48, (" svdss: OFFLINE (" + QHostInfo::localHostName() + ")").toUtf8());
+        mvwprintw(mainWindow, 0, 48, (" svdss[" + QString::number(getuid()) + "]: OFFLINE (" + QHostInfo::localHostName() + ")").toUtf8());
         wattroff(mainWindow, C_SS_STATUS_OFF);
     }
     wattron(mainWindow, C_STATUS);
